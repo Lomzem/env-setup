d39105ca962dc2bd49cf5c38e3abcf9e9572ee94
Lawjay Lee <lawjaylee1@icloud.com>
2025-01-27 14:47:23 -0800

    zoxide

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ mkdir -p ~/.config/tmux
 mv ./src/tmux.conf ~/.config/tmux/tmux.conf
 mv ./src/nvim ~/.config/nvim
 mv ./src/zshrc.sh ~/.zshrc
+
+source ~/.zshrc
--- src/zshrc.sh
+++ src/zshrc.sh
@@ -62,4 +62,3 @@ autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
 
 zinit cdreplay -q
-eval "$(zoxide init zsh)"
