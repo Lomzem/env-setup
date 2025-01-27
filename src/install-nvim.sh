a0909d749d9ce8f68bec2007713df3edc4d9e8cf
Lawjay Lee <lawjaylee1@icloud.com>
2025-01-27 14:39:01 -0800

    moved

--- src/install-nvim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-86_64.appimage
-chmod u+x nvim-linux-x86_64.appimage
-./nvim-linux-x86_64.appimage
-
-mkdir -p /opt/nvim
-mv nvim-linux-x86_64.appimage /opt/nvim/nvim
