2934f280d46d381af602ce53b53f9a428367bfb8
Lawjay Lee <lawjaylee1@icloud.com>
2025-01-28 10:43:45 -0800

    lsp

--- src/nvim/lua/lomzem/plugins/colorschemes.lua
+++ src/nvim/lua/lomzem/plugins/colorschemes.lua
@@ -24,13 +24,7 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				-- floats = "transparent",
-			},
 		},
-		init = ":colorscheme tokyonight",
 	},
 
 	-- {
